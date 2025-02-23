1500f1c622999932a28fb384688a023c8cf09463
shownder <spowell83@gmail.com>
2025-02-23 12:42:59 -0800

    add bleeps

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,45 @@
 
 import { Animated } from '@arwes/react-animated';
 import { Animator } from '@arwes/react-animator';
-import { Dots } from '@arwes/react-bgs';
+import { Dots, GridLines, MovingLines } from '@arwes/react-bgs';
 import { FrameNefrex } from '@arwes/react-frames';
 import { Text } from '@arwes/react-text'
-import localFont from "next/font/local"; 
+import localFont from "next/font/local";
+import { type BleepsProviderSettings, BleepsProvider, useBleeps } from '@arwes/react-bleeps'
+// import { ReactNode } from 'react';
+import { BleepsOnAnimator } from '@arwes/react';
+
+type BleepsNames = 'click' | 'intro';
+
+// interface ButtonProps {
+//   name: BleepsNames
+//   children: ReactNode
+// };
+
+const bleepsSettings: BleepsProviderSettings<BleepsNames> = {
+  categories: {
+    background: { volume: 0.25 },
+    transition: { volume: 0.5 },
+    interaction: { volume: 0.75 },
+    notification: { volume: 1 }
+  },
+  bleeps: {
+    click: {
+      category: 'interaction',
+      sources: [
+        { src: './sounds/click.webm', type: 'audio/webm' },
+        { src: './sounds/click.mp3', type: 'audio/mpeg' }
+      ]
+    },
+    intro: {
+      category: 'notification',
+      sources: [
+        { src: './sounds/error.webm', type: 'audio/webm' },
+        { src: './sounds/error.mp3', type: 'audio/mpeg' }
+      ]
+    }
+  }
+}
 
 const aturesFont = localFont({
   src: './fonts/atures.ttf',
@@ -13,130 +48,43 @@ const aturesFont = localFont({
 
 export default function Home() {
   return (
-    <Animator active duration={{ enter: 2, exit: 2 }}>
-      <div style={{
-        position: 'absolute',
-        width: '100vw',
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        zIndex: -1
-      }}>
-        {/* Canvas element will ocupy the positioned parent element. */}
-
-        <Animator>
+    <BleepsProvider {...bleepsSettings}>
+      <Animator active duration={{ enter: 1, interval: 10 }}>
+        <div
+          style={{
+            position: 'absolute',
+            width: '100vw',
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            inset: 0,
+            backgroundColor: '#000906',
+            backgroundImage:
+              'radial-gradient(85% 85% at 50% 50%, hsla(185, 100%, 25%, 0.25) 0%, hsla(185, 100%, 25%, 0.12) 50%, hsla(185, 100%, 25%, 0) 100%)'
+          }}
+        >
           <div style={{ position: 'relative', width: 150, height: 100, display: 'flex', justifyContent: 'center', alignItems: 'center', zIndex: 1000 }}>
-            <FrameNefrex
-              style={{
-                // @ts-expect-error css variables
-                '--arwes-frames-bg-color': 'hsl(180, 75%, 10%)',
-                '--arwes-frames-line-color': 'hsl(180, 75%, 30%)',
-                '--arwes-frames-deco-color': 'hsl(180, 75%, 50%)'
-              }}
-            >
-              
-            </FrameNefrex>
             <Animator>
-              <Text as="p" className={`text-decipher ${aturesFont.className}`} manager='decipher' fixed style={{ color: '#ddd', textAlign: 'center'}}>INCOMING COMMUNICATION</Text>
+              <FrameNefrex
+                style={{
+                  // @ts-expect-error css variables
+                  '--arwes-frames-bg-color': 'hsl(180, 75%, 10%)',
+                  '--arwes-frames-line-color': 'hsl(180, 75%, 30%)',
+                  '--arwes-frames-deco-color': 'hsl(180, 75%, 50%)'
+                }}
+              />
+              <BleepsOnAnimator<BleepsNames> transitions={{ entering: 'intro' }} />
+              <Animator>
+                <Text as="p" className={`text-decipher ${aturesFont.className}`} manager='decipher' fixed style={{ color: '#ddd', textAlign: 'center'}}>INCOMING COMMUNICATION</Text>
+              </Animator>            
             </Animator>
           </div>
-        </Animator>
-
-        <Dots
-          color='hsla(133, 100.00%, 16.30%, 0.50)'
-        />
-
-        {/* <div className={styles.page}>
-          <main className={styles.main}>
-            <Image
-              className={styles.logo}
-              src="/next.svg"
-              alt="Next.js logo"
-              width={180}
-              height={38}
-              priority
-            />
-            <ol>
-              <li>
-                Get started by editing <code>app/page.tsx</code>.
-              </li>
-              <li>Save and see your changes instantly.</li>
-            </ol>
-
-            <div className={styles.ctas}>
-              <a
-                className={styles.primary}
-                href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Image
-                  className={styles.logo}
-                  src="/vercel.svg"
-                  alt="Vercel logomark"
-                  width={20}
-                  height={20}
-                />
-                Deploy now
-              </a>
-              <a
-                href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-                className={styles.secondary}
-              >
-                Read our docs
-              </a>
-            </div>
-          </main>
-
-          <footer className={styles.footer}>
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                aria-hidden
-                src="/file.svg"
-                alt="File icon"
-                width={16}
-                height={16}
-              />
-              Learn
-            </a>
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                aria-hidden
-                src="/window.svg"
-                alt="Window icon"
-                width={16}
-                height={16}
-              />
-              Examples
-            </a>
-            <a
-              href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                aria-hidden
-                src="/globe.svg"
-                alt="Globe icon"
-                width={16}
-                height={16}
-              />
-              Go to nextjs.org →
-            </a>
-          </footer>
-        </div> */}
-      </div>
-    </Animator>
+          <GridLines lineColor="hsla(180, 100%, 75%, 0.05)" distance={30} />
+          <Dots color="hsla(180, 100%, 75%, 0.05)" distance={30} />
+          <MovingLines lineColor="hsla(180, 100%, 75%, 0.07)" distance={30} sets={20} />
+        </div>
+      </Animator>
+    </BleepsProvider>
   );
 }
