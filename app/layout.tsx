ab0b63f480b93803e19552cc3414c2d41ef506df
shownder <spowell83@gmail.com>
2025-02-24 19:42:15 -0800

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,13 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Strfinder Society Communications",
   description: "Researching the gap and the galaxy!",
+  openGraph: {
+    title: "Strfinder Society Communications",
+    description: "Researching the gap and the galaxy!",
+    images: [
+      { url: "https://assets-us-01.kc-usercontent.com/d960ad1b-9967-00f9-1158-72274b18312d/0db0055c-357e-487d-81c5-6f17a5dbe227/SFPFS_Symbol_Scenarios_800x1000.png" },
+    ],
+  },
 };
 
 export default function RootLayout({
